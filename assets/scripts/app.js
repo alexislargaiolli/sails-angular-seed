7c82b435ec1695d8b6cd5a24f26b31e357637cb6
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2016-06-03 16:43:14 +0000

    Config angular in assets folder

---
+++ assets/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name assetsApp
+ * @description
+ * # assetsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myModule', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ assets/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assetsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the assetsApp
+ */
+angular.module('myModule')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
